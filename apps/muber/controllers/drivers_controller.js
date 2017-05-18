2a9809a090d3305de922a700e35b5fc961d13608
C. Jordan Ball III <cjordanball@gmail.com>
2017-05-18 10:16:31 -0400

    add routes and controllers for update and delete

--- apps/muber/controllers/drivers_controller.js
+++ apps/muber/controllers/drivers_controller.js
@@ -1,3 +1,4 @@
+
 module.exports = {
 	howdy(req, res) {
 		res.send({ hi: 'there' });
@@ -6,5 +7,27 @@ module.exports = {
 	create(req, res) {
 		console.log('Hello, new driver!', req.body);
 		res.send({ test: 'test' });
+	},
+
+	update(req, res, next) {
+		const driverId = req.params.id;
+		const driverOptions = req.body;
+		Driver.findByIdAndRemove(driverId, driverOptions)
+		.then(() => {
+			Driver.findById(driverId)
+			.then(driver => res.send(driver))
+			.catch(next);
+		});
+	},
+
+	delete(req, res, next) {
+		const driverId = req.params.id;
+
+		Driver.findByIdAndRemove(driverId)
+		.then(driver => res
+			.status(204)
+			.send(driver)
+		)
+		.catch(next);
 	}
 };
--- apps/muber/routes/routes.js
+++ apps/muber/routes/routes.js
@@ -4,4 +4,8 @@ module.exports = (app) => {
 	app.get('/api', DriversController.howdy);
 
 	app.post('/api/drivers', DriversController.create);
+
+	app.put('/api/drivers/:id', DriversController.update);
+
+	app.delete('/api/drivers/:id', DriversController.delete);
 };
